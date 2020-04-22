d577ad1a092a8bc469e94414c67cf88751bba218
Dave Burke <thoughtcriminall@gmail.com>
2020-04-22 18:01:30 -0500

    Add 4.3

--- djb/chapter04.js
+++ djb/chapter04.js
@@ -46,3 +46,48 @@ let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
+
+/* 4.3 a list */
+function arrayToList(arr) {
+  const head = { value: arr[0], rest: null }
+  let next = head;
+  for(let i = 1; i < arr.length; i++) {
+    next.rest = { value: arr[i], rest: null };
+    next = next.rest;
+  }
+  return head;
+}
+
+function listToArray(list) {
+  const arr = [list.value];
+  let rest = list.rest;
+  while(rest != null) {
+    arr.push(rest.value);
+    rest = rest.rest;
+  }
+  return arr;
+}
+
+function prepend(value, rest) {
+  return { value, rest };
+}
+
+function nth(list, index) {
+  let node = list;
+  for(let i = 0; i < index; i++) {
+    if(!node.rest) {
+      return undefined;
+    }
+    node = node.rest;
+  }
+  return node.value;
+}
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
