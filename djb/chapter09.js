cc6043013c6663f2fcdac6a48c6902d670e0225d
Dave Burke <thoughtcriminall@gmail.com>
2020-05-07 22:13:50 -0500

    Update chapter09.js

--- djb/chapter09.js
+++ djb/chapter09.js
@@ -53,3 +53,20 @@ function verify(regexp, yes, no) {
 let text = "'I'm the cook,' he said, 'it's my job.'";
 console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2')); // I looked at this answer
 // → "I'm the cook," he said, "it's my job."
+
+/* 9.3 Numbers again */
+// Fill in this regular expression.
+let number = /^[-+]?(\d\.?\d*|\d*\.\d)(e[-+]?\d+)?$/i;
+// Official:   [+\-]?(\d+(\.\d*)?|\.\d+)([eE][+\-]?\d+)?
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4", "1e+12"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5", ".5.", "1f5", "."]) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
