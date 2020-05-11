a891a34c267918e9f3bd457dd4567809ec326ce2
Dave Burke <thoughtcriminall@gmail.com>
2020-05-11 12:38:26 -0500

    Update chapter11.js

--- djb/chapter11.js
+++ djb/chapter11.js
@@ -11,8 +11,58 @@ async function locateScalpel(nest) {
 }
 
 function locateScalpel2(nest) {
-  // Your code here.
+  const lookup = function(name) {
+    return anyStorage(nest, name, 'scalpel').then(scalpel => {
+      if(scalpel === name) {
+        return scalpel;
+      } else {
+        return lookup(scalpel);
+      }
+    });
+  }
+  return lookup(nest.name);
 }
 
 locateScalpel(bigOak).then(console.log);
+locateScalpel2(bigOak).then(console.log);
 // → Butcher Shop
+
+/* 9.2 Building Promis.all */
+function Promise_all(promises) {
+  return new Promise((resolve, reject) => {
+    let nResolved = 0;
+    let results = [];
+    if(promises.length == 0) resolve([]);
+    promises.forEach((promise, i) => {
+      promise.then(result => {
+        results[i] = result;
+        nResolved++;
+        if(nResolved == promises.length) {
+          resolve(results);
+        }
+      }).catch(err => reject(err));
+    });
+  });
+}
+
+// Test code.
+Promise_all([]).then(array => {
+  console.log("This should be []:", array);
+});
+function soon(val) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(val), Math.random() * 500);
+  });
+}
+Promise_all([soon(1), soon(2), soon(3)]).then(array => {
+  console.log("This should be [1, 2, 3]:", array);
+});
+Promise_all([soon(1), Promise.reject("X"), soon(3)])
+  .then(array => {
+    console.log("We should not get here");
+  })
+  .catch(error => {
+    if (error != "X") {
+      console.log("Unexpected failure:", error);
+    }
+  });
